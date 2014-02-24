13ca540d6c80c41dfcb066a4d96fd5ff4e2ee514
Nite Luo <darkdukey@gmail.com>
2014-02-24 15:43:59 -0800

    generate class IDs

--- targets/csx/templates/layout_foot.h
+++ targets/csx/templates/layout_foot.h
@@ -1,2 +1,9 @@
 
+## ----- Generate Hashed IDs for cocos2d-x classes
+#for csxclass in $sorted_classes
+	#if $in_listed_classes(csxclass)
+\#define	k${csxclass} = HASH("${csxclass}")
+	#end if
+#end for
+
 \#endif
--- targets/csx/templates/prelude.h
+++ targets/csx/templates/prelude.h
@@ -1,3 +1,3 @@
-#set generator = $current_class.generator
 
+#set generator = $current_class.generator
 void csx_register_${current_class.class_name}();
