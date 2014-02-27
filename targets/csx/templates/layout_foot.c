040d2424e3349f3046caf9f3d5f3697f287c87d7
Nite Luo <darkdukey@gmail.com>
2014-02-26 16:30:58 -0800

    register constructors

--- targets/csx/templates/layout_foot.c
+++ targets/csx/templates/layout_foot.c
@@ -6,6 +6,14 @@ void csx_register_all_${prefix}()
 	csx_register_${csxclass}();
 	#end if
 	#end for
+
+	## ----- Register class
+	auto classFactory = CSX()->getService<ClassFactoryServiceInterface>();
+	#for csxclass in $sorted_classes
+	#if $in_listed_classes(csxclass)
+	classFactory->registerFactory(HASH("${csxclass}"), createCocos2dClass);
+	#end if
+	#end for
 }
 
 ## ----- End of namespace
