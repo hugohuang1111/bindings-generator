3af5694ff38e9eae5fb73a21812a3f7bf6f40052
Nite Luo <darkdukey@gmail.com>
2015-04-13 13:44:30 -0700

    make js binding works with older spidermonkey version

--- targets/spidermonkey/templates/prelude.h
+++ targets/spidermonkey/templates/prelude.h
@@ -3,7 +3,13 @@
 extern JSClass  *jsb_${current_class.underlined_class_name}_class;
 extern JSObject *jsb_${current_class.underlined_class_name}_prototype;
 
-bool js_${generator.prefix}_${current_class.class_name}_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_${generator.prefix}_${current_class.class_name}_finalize(JSContext *cx, JSObject *obj);
+\#if MOZJS_MAJOR_VERSION >= 33
 void js_register_${generator.prefix}_${current_class.class_name}(JSContext *cx, JS::HandleObject global);
 void register_all_${generator.prefix}(JSContext* cx, JS::HandleObject obj);
+\#else
+void js_register_${generator.prefix}_${current_class.class_name}(JSContext *cx, JSObject* global);
+void register_all_${generator.prefix}(JSContext* cx, JSObject* obj);
+\#endif
+
+bool js_${generator.prefix}_${current_class.class_name}_constructor(JSContext *cx, uint32_t argc, jsval *vp);
+void js_${generator.prefix}_${current_class.class_name}_finalize(JSContext *cx, JSObject *obj);
